ce533dac7eb103bbeda5c3bc952cbf83eab05e22
BOT-MAKES162-crypto <Exsanaff@gmail.com>
2025-03-12 21:00:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=blggXARQ#eUOQKJt-oP-IRfMGi_FAZEO3zU3BUa2KdgIB2SjKIrw",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=mxpGyKaY#MWf5V-v0tq9E-GT44AZQjMtoiT0zJtGpvRUTLOjMKtI",
 MONGODB: process.env.MONGODB|| "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya",
 };
